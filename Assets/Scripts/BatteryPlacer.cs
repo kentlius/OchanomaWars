c562530eee63eb15bf72c6da287b018c0d5f591e
ElectroDragon <electrodragon@me.com>
2013-07-17 07:16:33 +0900

    Added restriction that player con only put new battery on free space.

--- Assets/Scripts/BatteryPlacer.cs
+++ Assets/Scripts/BatteryPlacer.cs
@@ -6,22 +6,49 @@ public class BatteryPlacer : MonoBehaviour {
     public Transform batteryRoot;
     public GameObject batteryPrefab;
 
+    public Light cursor;
+    Transform cursorTransform;
+
+    public LayerMask floorLayer = 1 << 11;
+
+    void Start () {
+        cursorTransform = cursor.GetComponent<Transform>();
+    }
+
     void Update () {
-        if (batteryPrefab != null && Input.GetMouseButton(0)) {
-            PlaceBattery();
+        if (batteryPrefab == null)
+            return;
+
+        /* ドラッグ */
+        if (Input.GetMouseButton(0)) {
+            PlaceCursor();
+        }
+        if (Input.GetMouseButtonUp(0) && cursor.color == Color.green) {
+            PlaceBatteryToCursorPosition();
         }
     }
 
-    void PlaceBattery () {
-        RaycastHit hit = new RaycastHit();
+    void PlaceCursor () {
+        RaycastHit hitPlace = new RaycastHit();
         Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast (cameraRay.origin, cameraRay.direction, out hit,
+        if (Physics.Raycast (cameraRay.origin, cameraRay.direction, out hitPlace,
                              Mathf.Infinity)) {
-            Transform batteryTransform = (GameObject.Instantiate(batteryPrefab) as
-                                          GameObject).GetComponent<Transform>();
-            batteryTransform.parent = batteryRoot;
-            batteryTransform.position = hit.point;
-            batteryPrefab = null;
+            /* カーソルを配置 */
+            cursorTransform.position = hitPlace.point + Vector3.up * 100;
+            /* カーソルが砲台を置けない場所を指していたら */
+            if (((1 << hitPlace.collider.gameObject.layer) & floorLayer) == 0) {
+                cursor.color = Color.red;
+            } else {
+                cursor.color = Color.green;
+            }
         }
+    }
+
+    void PlaceBatteryToCursorPosition () {
+        Transform batteryTransform = (GameObject.Instantiate(batteryPrefab) as
+                                      GameObject).GetComponent<Transform>();
+        batteryTransform.parent = batteryRoot;
+        batteryTransform.position = cursorTransform.position + Vector3.down * 100;
+        batteryPrefab = null;
     }
 }
